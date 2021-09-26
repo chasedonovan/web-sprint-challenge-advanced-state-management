a785e1d5af7ed6b1bc0b5b0faff6690594685ddc
Chase Donovan <chase.donovan@aol.com>
2021-09-25 19:04:39 -0700

    almost done

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import {addSmurfs, errorMessage } from '../actions';
+import {connect} from 'react-redux';
 
 const AddForm = (props) => {
     const [state, setState] = useState({
@@ -9,7 +11,7 @@ const AddForm = (props) => {
     });
 
     //remove when error state is added
-    const errorMessage = "";
+    const errorMessage = state.errorMessage;
 
     const handleChange = e => {
         setState({
@@ -21,8 +23,10 @@ const AddForm = (props) => {
     const handleSubmit = e => {
         e.preventDefault();
         if (state.name === "" || state.position === "" || state.nickname === "") {
-            //add in error action
-        }
+            props.errorMessage("Name, position and nickname fields are required.");
+        } 
+        else{
+            props.addSmurfs(state)        }
     }
 
     return(<section>
@@ -47,12 +51,12 @@ const AddForm = (props) => {
             {
                 errorMessage && <div data-testid="errorAlert" className="alert alert-danger" role="alert">Error: {errorMessage}</div>
             }
-            <button>Submit Smurf</button>
+            <button onClick={handleSubmit}>Submit Smurf</button>
         </form>
     </section>);
 }
 
-export default AddForm;
+export default connect(null,{addSmurfs,errorMessage})(AddForm);
 
 //Task List:
 //1. Connect the errorMessage, setError and addSmurf actions to the AddForm component.
